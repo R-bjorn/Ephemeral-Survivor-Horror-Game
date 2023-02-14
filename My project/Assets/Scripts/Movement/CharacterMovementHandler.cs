c130af238c5a836ff4cb24234decdf3ebb7ec538
Jason Cala <calaj@uwindsor.ca>
2023-02-13 22:24:01 -0500

    Added more input scripts

---
+++ My project/Assets/Scripts/Movement/CharacterMovementHandler.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Fusion;
+
+public class CharacterMovementHandler : NetworkBehaviour
+{
+    // Other components
+    NetworkCharacterControllerPrototypeCustom networkCharacterControllerPrototypeCustom;
+
+    private void Awake()
+    {
+        networkCharacterControllerPrototypeCustom = GetComponent<NetworkCharacterControllerPrototypeCustom>();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public override void FixedUpdateNetwork()
+    {
+        // Get the input from the network
+        if (GetInput(out NetworkInputData networkInputData))
+        {
+            //Move
+            Vector3 moveDirection = transform.forward * networkInputData.movementInput.y + transform.right * networkInputData.movementInput.x;
+            moveDirection.Normalize();
+
+            networkCharacterControllerPrototypeCustom.Move(moveDirection);
+        }
+    }
+}
