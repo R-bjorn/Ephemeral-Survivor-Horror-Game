a41c7ebec6a2ad5dbbac380697ea5545e1b5dab6
r-bjorn <ravidtrivedi111@gmail.com>
2023-02-17 21:09:50 -0500

    Updating UI Audio

--- My project/Assets/Scripts/MenuMainUI.cs
+++ My project/Assets/Scripts/MenuMainUI.cs
@@ -5,11 +5,43 @@ using UnityEngine.UI;
 
 public class MenuMainUI : MonoBehaviour
 {
-    [SerializeField][Tooltip("Audio upon clicking the button")]
-    private AudioSource enterClip;
+    // [SerializeField][Tooltip("Audio play upon mouse enter")]
+    // private AudioClip mouseEnter;
+    // [SerializeField][Tooltip("Audio play upon mouse click ")]
+    // private AudioClip mouseClick;
+    //
+    // private bool _buttonClicked = false;
+    // private AudioSource _audioSource;
+    //
+    // private void Start()
+    // {
+    //     _audioSource = GetComponent<AudioSource>();
+    // }
+    //
+    // private void OnMouseOver()
+    // {
+    //     Debug.Log("Enter mouse");
+    //     if (!_buttonClicked)
+    //     {
+    //         _audioSource.clip = mouseEnter;
+    //         _audioSource.Play();
+    //     }
+    // }
+    //
+    // private void OnMouseDown()
+    // {
+    //     _buttonClicked = true;
+    //     _audioSource.clip = mouseClick;
+    //     _audioSource.Play();
+    // }
+    //
+    // private void OnMouseUp()
+    // {
+    //     _buttonClicked = false;
+    // }
+
     public void MoveToScene(int scenetoChange)
     {
-        enterClip.Play();
         SceneManager.LoadScene(scenetoChange);
     }
 
