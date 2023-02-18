0b82c0402413b906bcee61fe5b825fca3bd006e0
r-bjorn <ravidtrivedi111@gmail.com>
2023-02-17 20:36:20 -0500

    Adding UI audios

--- My project/Assets/Scripts/MenuMainUI.cs
+++ My project/Assets/Scripts/MenuMainUI.cs
@@ -5,8 +5,11 @@ using UnityEngine.UI;
 
 public class MenuMainUI : MonoBehaviour
 {
+    [SerializeField][Tooltip("Audio upon clicking the button")]
+    private AudioSource enterClip;
     public void MoveToScene(int scenetoChange)
     {
+        enterClip.Play();
         SceneManager.LoadScene(scenetoChange);
     }
 
