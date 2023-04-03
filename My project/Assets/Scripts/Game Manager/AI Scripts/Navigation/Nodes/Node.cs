c03e6d3d1ca50569659faee70310a283d8e8f4ca
r-bjorn <ravidtrivedi111@gmail.com>
2023-04-03 18:29:35 -0400

    Remove AI Scripts

--- My project/Assets/Scripts/Game Manager/AI Scripts/Navigation/Nodes/Node.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace Game_Manager.AI_Scripts.Navigation.Nodes
-{
-    /// <summary>
-    /// These manually placed will be connected by an agent manager but have no other logic themselves.
-    /// </summary>
-    public class Node : NodeBase { }
-}
