10a2f593a2972290089b43aa32f57cf1c783920c
Jason Cala <calaj@uwindsor.ca>
2023-04-15 19:03:39 -0400

    Added crystal prefab

---
+++ My project/Assets/Scripts/Game Manager/Mind/Pickups/Crystal.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Crystal : MonoBehaviour
+{
+    public static Crystal Instance { get; private set; }
+    public int crystalValue = 1;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            Player player = other.GetComponent<Player>();
+            if (player != null)
+            {
+                player.addScore(crystalValue);
+                Destroy(gameObject);
+            }
+            else
+            {
+
+            }
+        }
+        else
+        {
+
+        }
+    }
+}
