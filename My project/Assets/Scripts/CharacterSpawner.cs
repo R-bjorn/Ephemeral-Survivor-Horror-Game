7c47196c36ae1bca811c5cb4be5fd8f14419216f
Jason Cala <calaj@uwindsor.ca>
2023-04-14 21:47:20 -0400

    Fixed base multiplayer

--- My project/Assets/Scripts/CharacterSpawner.cs
+++ My project/Assets/Scripts/CharacterSpawner.cs
@@ -22,7 +22,7 @@ public class CharacterSpawner : NetworkBehaviour
             if (character != null)
             {
                 Debug.Log("3");
-                var spawnPos = new Vector3(Random.Range(-3f, 3f), 0f, Random.Range(-3f, 3f));
+                var spawnPos = new Vector3(Random.Range(-41f, -35f), 4f, Random.Range(125f, 132f));
                 var characterInstance = Instantiate(character.GameplayPrefab, spawnPos, Quaternion.identity);
                 characterInstance.SpawnAsPlayerObject(client.Value.clientId);
                 Debug.Log("4");
